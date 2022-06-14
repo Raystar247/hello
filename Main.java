5b1c48e63101e6c3b29c5f3511903f8d0e2662e0
Ren-SAKAI <1895036t@stu.kobe-u.ac.jp>
2022-06-14 22:56:10 +0900

    Revise the part

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,20 +7,34 @@ public class Main {
     public static void main(String[] args) {
         try (Scanner scan = new Scanner(System.in)) {
             Random rand = new Random();
-            int target = rand.nextInt(100);
+            int target = rand.nextInt(90) + 10;
+            
             for (int i = 0; i < 5; i++) {
-                System.out.print("入力してください:  ");
-                int num = scan.nextInt();
-                if ( Math.abs(num - target) >= 20 ) {
-                    System.out.print("20以上");
-                }
-                if ( num > target ) {
-                    System.out.println("多いです");
-                } else if ( num < target ) {
-                    System.out.println("少ないです");
-                } else {
-                    System.out.println("正解");
-                    break;
+                int num;
+                try {
+                    while(true) {
+                        try {
+                            System.out.print("Answer "+ (i+1) + "  => Input the number: ");
+                            num = scan.nextInt();
+                            break;
+                        } catch(InputMismatchException e) {
+                            System.out.println("Incorrect Input");
+                            scan.next();
+                        }
+                    }
+                    if ( Math.abs(num - target) >= 20 ) {
+                        System.out.print("20以上");
+                    }
+                    if ( num > target ) {
+                        System.out.println("大きいです");
+                    } else if ( num < target ) {
+                        System.out.println("小さいです");
+                    } else {
+                        System.out.println("正解");
+                        break;
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println(e);
                 }
                 if (i == 4) {
                     String s = "失敗  正解:" + target;
@@ -27,6 +42,8 @@ public class Main {
                 }
              }
             scan.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
